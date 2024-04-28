c004658de78d4a35ad60223c1d9bcddfc8514f7c
CEREN ALTUNBAŞ <86134934+crnaltunbas@users.noreply.github.com>
2024-04-28 11:25:37 +0300

    Update feature_engineering.py
    Aykırı değer problemini çözdük ve bir recap yaptık.

--- feature_engineering.py
+++ feature_engineering.py
@@ -195,3 +195,86 @@ check_outlier(df, "Age")
 grab_outliers(df, "Age", True)
 
 
+# Aykırı değer Problemini Çözme
+
+# Silme
+low, up = outlier_thresholds(df, "Fare")
+df.shape
+
+df[~((df["Fare"] < low) | (df["Fare"] > up))].shape
+
+def remove_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    df_without_outliers = dataframe[~((dataframe[col_name] < low_limit) | (dataframe[col_name] > up_limit))]
+    return df_without_outliers
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+num_cols = [col for col in num_cols if col not in "PassengerId"]
+
+df.shape
+
+for col in num_cols:
+    new_df = remove_outlier(df, col)
+
+df.shape[0] - new_df.shape[0]
+
+
+# Baskılama Yöntemi (re-assignment with thresholds)
+
+low, up = outlier_thresholds(df, "Fare")
+
+df[((df["Fare"] < low) | (df["Fare"] > up))]["Fare"]
+
+df.loc[((df["Fare"] < low) | (df["Fare"] > up)), "Fare"]
+
+df.loc[(df["Fare"] > up), "Fare"] = up
+# Aykırı değerleri up değerler ile değiştirmiş olduk
+
+df.loc[(df["Fare"] < low), "Fare"] = low
+
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+df = load()
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+num_cols = [col for col in num_cols if col not in "PassengerId"]
+
+df.shape
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+for col in num_cols:
+    replace_with_thresholds(df, col)
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+# RECAP
+
+df = load()
+# Okuma işlemi yapıldı
+outlier_thresholds(df, "Age")
+# çok önemli
+# Aykırı değerleri saptadık
+check_outlier(df, "Age")
+# Bu threshold-lara göre aykırı değer var mı yok mu diye sorduk
+grab_outliers(df, "Age", index=True)
+# çok önemli
+# Aykırı değerleri bize getir dedik
+
+remove_outlier(df, "Age").shape
+# Aykırı değerleri silelim dedik
+replace_with_thresholds(df, "Age")
+# çok önemli
+# Aykırı değerlerin yerine başka değerler koyduk
+check_outlier(df, "Age")
+# Tekrar gözlemledik.
+
+
+
