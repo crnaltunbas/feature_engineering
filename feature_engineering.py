b558ef209ab023a4ca534e914fb3632ad96548ba
CEREN ALTUNBAŞ <86134934+crnaltunbas@users.noreply.github.com>
2024-04-28 12:11:50 +0300

    Update feature_engineering.py
    Çok değişkenli aykırı değer analizi yapıldı(LOF YÖNTEMİ)

--- feature_engineering.py
+++ feature_engineering.py
@@ -276,5 +276,53 @@ replace_with_thresholds(df, "Age")
 check_outlier(df, "Age")
 # Tekrar gözlemledik.
 
+# ÇOK DEĞİŞKENLİ AYKIRI DEĞER ANALİZİ : LOCAL OUTLİER FACTOR
 
+# LOF yöntemi gözlemleri yoğunluk tabanlı  skorlayarak buna göre aykırı değer tanımı yapabilmemizi sağlar.
+
+
+df = sns.load_dataset('diamonds')
+df = df.select_dtypes(include=['float64', 'int64'])
+df = df.dropna()
+df.head()
+df.shape
+
+for col in df.columns:
+    print(col, check_outlier(df, col))
+
+
+low, up = outlier_thresholds(df, "carat")
+
+df[((df["carat"] < low) | (df["carat"] > up))].shape
+
+low, up = outlier_thresholds(df, "depth")
+
+df[((df["depth"] < low) | (df["depth"] > up))].shape
+
+clf = LocalOutlierFactor(n_neighbors=20)
+clf.fit_predict(df)
+
+df_scores = clf.negative_outlier_factor_
+df_scores[0:5]
+# df_scores = -df_scores
+np.sort(df_scores)[0:5]
+
+
+scores = pd.DataFrame(np.sort(df_scores))
+scores.plot(stacked=True, xlim=[0, 50], style='.-')
+plt.show()
+
+
+th = np.sort(df_scores)[3]
+
+df[df_scores < th]
+
+df[df_scores < th].shape
+
+
+df.describe([0.01, 0.05, 0.75, 0.90, 0.99]).T
+
+df[df_scores < th].index
+
+df[df_scores < th].drop(axis=0, labels=df[df_scores < th].index)
 
